a8d03d7c1b46186d6a26c07b4f5f3d1ff3d56b21
Ryan Kleffman <91687869+ryanq47@users.noreply.github.com>
2022-01-27 11:56:37 -0600

    Add files via upload

--- 1.2.sh
+++ 1.2.sh
@@ -12,7 +12,7 @@ passwd root
 echo root password is changed
 
 echo listing all users with UID above 999
-cat /etc/passwd | grep '[0-9][0-9][0-9]'
+cat /etc/passwd | grep '1[0-9][0-9][0-9]'
 
 read -p "Press enter to continue"
 
@@ -34,3 +34,46 @@ echo $(date): using chattr +i to lock files >> /var/log/ryanlog.log
 	chattr +i /etc/gpasswd
 	chattr +i /etc/group
 	chattr +i /etc/inittab
+	
+read -p "Press enter to delete unused media"
+
+	
+echo Getting rid of media 
+echo $(date): Logging media >> /var/log/ryanlog.log
+
+	find / -name "*.mp3" -type f >> /var/log/ryanlog.log
+	find / -name "*.wav" -type f >> /var/log/ryanlog.log
+	find / -name "*.wmv" -type f >> /var/log/ryanlog.log
+	find / -name "*.mp4" -type f >> /var/log/ryanlog.log
+	find / -name "*.mov" -type f >> /var/log/ryanlog.log
+	find / -name "*.avi" -type f >> /var/log/ryanlog.log
+	find / -name "*.mpeg" -type f >> /var/log/ryanlog.log
+	find /home -name "*.jpeg" -type f >> /var/log/ryanlog.log
+	find /home -name "*.jpg" -type f >> /var/log/ryanlog.log
+	find /home -name "*.png" -type f >> /var/log/ryanlog.log
+	find /home -name "*.gif" -type f >> /var/log/ryanlog.log
+	find /home -name "*.tif" -type f >> /var/log/ryanlog.log
+	find /home -name "*.tiff" -type f >> /var/log/ryanlog.log
+	find / -name "*.mp3" -type f -delete
+	find / -name "*.wav" -type f -delete
+	find / -name "*.wmv" -type f -delete
+	find / -name "*.mp4" -type f -delete
+	find / -name "*.mov" -type f -delete
+	find / -name "*.avi" -type f -delete
+	find / -name "*.mpeg" -type f -delete
+	find /home -name "*.jpeg" -type f -delete
+	find /home -name "*.jpg" -type f -delete
+	find /home -name "*.png" -type f -delete
+	find /home -name "*.gif" -type f -delete
+	find /home -name "*.tif" -type f -delete
+	find /home -name "*.tiff" -type f -delete
+	
+read -p "Press enter to install RKhunter"
+
+	dnf install rkhunter -y
+
+	rkhunter –propupd
+
+	rkhunter --check
+
+	echo results logged at /var/log/rkhunter/rkhunter.log
